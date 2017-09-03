2d71d3c74bfcf2a347fac909dfa1ffb469a2d526
Oscar Servia Gonzalez <osgonzalez@esei.uvigo.es>
2017-09-03 18:01:35 +0200

    Create the basic player GUI
    -Adds the life interface.
    -Adds the defeat menu.

--- Assets/Scrips/levelmainScript.cs
+++ Assets/Scrips/levelmainScript.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class levelmainScript : MonoBehaviour {
 
 
 	public int playerLife = 100;
+	public Text lifeText;
+	public Button defeatButton;
+	public string lifeTextMessage = "Life: ";
+	private int originalLife =0; 
 
 	// Use this for initialization
 	void Start () {
-		
+		defeatButton.gameObject.SetActive (false);
+		originalLife = playerLife;
+		lifeText.text = lifeTextMessage + playerLife + "/" + originalLife;
 	}
 	
 	// Update is called once per frame
@@ -19,12 +27,24 @@ public class levelmainScript : MonoBehaviour {
 
 	public void doDamage(int damage){
 		playerLife -= damage;
+
 		if (playerLife <= 0) {
 			failLevel ();
+			playerLife = 0;
 		}
+
+		lifeText.text = lifeTextMessage + playerLife + "/" + originalLife;
+	}
+
+	public void restartLevel(){
+		Time.timeScale = 1;
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
 	}
 
 	void failLevel(){
+		defeatButton.gameObject.SetActive (true);
+		Time.timeScale = 0;
+		
 	}
 
 }
